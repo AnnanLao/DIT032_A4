f11a3af79ff1e1aba30618ca6308dcaf8d94e1c9
AnnanLao <55920573+AnnanLao@users.noreply.github.com>
2020-03-10 05:27:14 +0100

    Update queries.js

--- queries.js
+++ queries.js
@@ -3,38 +3,72 @@ db.conferences.count()
 
 //MGDB3.2
 db.conferences.find({}, {
-    name: 1,
-    _id: 1
+	name: 1,
+	_id: 1
 });
 
 //MGDB3.3
-db.delegates.distinct( "country" )
+db.delegates.distinct("country")
 
 //MGDB3.4
-db.conferences.find( { tracks: { $elemMatch: { topic: "practice" } } })
+db.conferences.find({
+	tracks: {
+		$elemMatch: {
+			topic: "practice"
+		}
+	}
+})
 
 //MGDB3.5
-db.conferences.find({"duration.begin": {$gt: new ISODate("2019-03-31T23:59:59Z"), $lt:new ISODate("2019-05-01T00:00:00Z")}})
+db.conferences.find({
+	"duration.begin": {
+		$gt: new ISODate("2019-03-31T23:59:59Z"),
+		$lt: new ISODate("2019-05-01T00:00:00Z")
+	}
+})
 
 //MGDB3.6
-db.delegates.find( { $where: "this.attends.length > 1" } );
+db.delegates.find({
+	$where: "this.attends.length > 1"
+});
 
 //MGDB3.7
-db.delegates.find( { $or: [ { name: { $regex: /^B/i } }, { name: { $regex: /^S/i } } ] })
+db.delegates.find({
+	$or: [{
+		name: {
+			$regex: /^B/i
+		}
+	}, {
+		name: {
+			$regex: /^S/i
+		}
+	}]
+})
 
 // MGDB3.8
-db.conference.update({ "name" : "MSR'16"}, {$set: {"general_chair": "Gregg Rothermel"}})
+db.conference.update({
+	"name": "MSR'16"
+}, {
+	$set: {
+		"general_chair": "Gregg Rothermel"
+	}
+})
 
 // MGDB3.9
-db.conference.remove({"location.country" :"India"}) 
+db.conference.remove({
+	"location.country": "India"
+})
 
 // MGDB3.10
-db.conferences.aggregate({name, edition}
-[
-{$lookup:{
-from:"delegates",
-localField: "general_chair",
-foreignField:"name"}
-}
-]
+db.conferences.aggregate({
+		name,
+		edition
+	}
+	[{
+		$lookup: {
+			from: "delegates",
+			localField: "general_chair",
+			foreignField: "name"
+		}
+	}]
 )
